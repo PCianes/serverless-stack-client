f64fd159155f91d794938465f6deca9f30af5dcf
PCianes <cianespablo@gmail.com>
2020-09-01 12:24:08 +0200

    🎉 Setup App with our AWS backend with Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-1",
+    BUCKET: "notes-app-uploads-demodev",
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://4q2a7sdak4.execute-api.eu-west-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_ZYJJFaQMt",
+    APP_CLIENT_ID: "5rm2hhbn60hlutj4b6ja0uibvt",
+    IDENTITY_POOL_ID: "eu-west-1:320a1935-d692-440c-a339-767d84b00836",
+  },
+};
